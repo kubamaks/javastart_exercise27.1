381ced0ad417ffa5f3abd6057eeba33c0945a551
kuba <kubamachnik@gmail.com>
2022-07-20 11:05:19 +0200

    project initialization

---
+++ src/main/java/pl/km/exercise271/Exercise271Application.java
@@ -0,0 +1,13 @@
+package pl.km.exercise271;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise271Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise271Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/km/exercise271/Exercise271ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.km.exercise271;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise271ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
